0e71cffadae582febecd6f61c6bbac912065e3d7
MinxinSU <sian415@gmail.com>
2021-12-06 23:50:39 +0800

    RemoveElement

--- Program.cs
+++ Program.cs
@@ -3,6 +3,8 @@
         static void Main(string[] args) {
             //new SumPair().start();
             new RemoveDuplicates().start(new int[] { 1, 2, 3, 3, 3, 4, 5, 6, 6, 6 });
+            new RemoveElement().start(new int[] { 0, 1, 2, 2, 3, 0, 4, 2 }, 2);
+            new RemoveElement().start(new int[] { 2 }, 3);
         }
     }
 }
---
+++ RemoveElement.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace LeetCode {
+    ///<summary> 移除陣列指定項目 </summary> 
+    public class RemoveElement {
+        public int start(int[] nums, int val) {
+            if (nums == null) {
+                return 0;
+            }
+
+            int len = nums.Length;
+            int flag = -1;
+            for (int i = 0; i < len; i++) {
+                if (nums[i] == val) {
+                    if (flag == -1) {
+                        flag = i;
+                    }
+                } else if (flag >= 0) {
+                    nums[flag] = nums[i];
+                    flag++;
+                }
+            }
+            return flag < 0 ? len : flag;
+        }
+    }
+}
