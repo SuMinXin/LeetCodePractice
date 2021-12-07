495aeef0d7194dbda81d010f98a3ab31f47bdbb7
MinxinSU <sian415@gmail.com>
2021-12-07 22:48:55 +0800

    StrStr

--- Program.cs
+++ Program.cs
@@ -2,9 +2,10 @@
     class Program {
         static void Main(string[] args) {
             //new SumPair().start();
-            new RemoveDuplicates().start(new int[] { 1, 2, 3, 3, 3, 4, 5, 6, 6, 6 });
-            new RemoveElement().start(new int[] { 0, 1, 2, 2, 3, 0, 4, 2 }, 2);
-            new RemoveElement().start(new int[] { 2 }, 3);
+            //new RemoveDuplicates().start(new int[] { 1, 2, 3, 3, 3, 4, 5, 6, 6, 6 });
+            //new RemoveElement().start(new int[] { 0, 1, 2, 2, 3, 0, 4, 2 }, 2);
+            //new RemoveElement().start(new int[] { 2 }, 3);
+            new StrString().start("mississippi", "pi");
         }
     }
 }
---
+++ StrStr.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace LeetCode {
+    ///<summary> 尋找指定項目字串 </summary> 
+    public class StrString {
+        public int start(string haystack, string needle) {
+            if (needle == "") {
+                return 0;
+            }
+            int t = needle.Length, len = haystack.Length;
+            int count = 0;
+            for (int i = 0; i < len; i++) {
+                if (count == 0 && (len - i) < t) {
+                    return -1;
+                } else {
+                    if (haystack[i] == needle[count]) {
+                        count++;
+                        if (count == t) {
+                            return i + 1 - t;
+                        }
+                    } else {
+                        i = i - count;
+                        count = 0;
+                    }
+                }
+            }
+            return -1;
+        }
+    }
+}
