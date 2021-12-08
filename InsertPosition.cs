e46401c025ffc501604af1b7bc909a98cfa437ed
MinxinSU <sian415@gmail.com>
2021-12-08 21:33:42 +0800

    find insert position

---
+++ InsertPosition.cs
@@ -0,0 +1,52 @@
+﻿namespace LeetCode {
+    ///<summary> 尋找插入位置 </summary> 
+    public class InsertPosition {
+        public int start(int[] nums, int target) {
+            int end = nums.Length;
+            int i = 0;
+            while (i < end) {
+                if (nums[i] >= target) {
+                    return i;
+                }
+                i++;
+            }
+            return end;
+        }
+
+        public int cutInHalf(int[] nums, int target) {
+            if (nums == null || nums[0] >= target) {
+                return 0;
+            }
+            int len = nums.Length;
+            if (nums[len - 1] < target) {
+                return len;
+            }
+            if (nums[len - 1] == target) {
+                return len - 1;
+            }
+
+            int position = len / 2;
+            int move = len / 2; // 移動幅度
+            for (int i = 0; i < (len / 2) + 1; i++) {
+                if (nums[position] == target) {
+                    return position;
+                }
+                move = move / 2;
+                if (move == 0) {
+                    if (nums[position] < target) {
+                        return position + 1;
+                    } else if (nums[position - 1] == target) {
+                        return position - 1;
+                    }
+                    return position;
+                }
+                if (nums[position] > target) {
+                    position -= move;
+                } else {
+                    position += move;
+                }
+            }
+            return position;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,18 +8,19 @@ namespace LeetCode {
             //new RemoveDuplicates().start(new int[] { 1, 2, 3, 3, 3, 4, 5, 6, 6, 6 });
             //new RemoveElement().start(new int[] { 0, 1, 2, 2, 3, 0, 4, 2 }, 2);
             //new RemoveElement().start(new int[] { 2 }, 3);
-            Stopwatch t = new Stopwatch();
-            StrString model = new StrString();
-            t.Start();
-            model.start("mississippi", "pi");
-            model.start("hello", "ll");
-            t.Stop();
-            Console.WriteLine($"front:{t.ElapsedMilliseconds}/ms");
-            t.Restart();
-            model.sample("mississippi", "pi");
-            model.sample("hello", "ll");
-            t.Stop();
-            Console.WriteLine($"back:{t.ElapsedMilliseconds}/ms");
+            // Stopwatch t = new Stopwatch();
+            // StrString model = new StrString();
+            // t.Start();
+            // model.start("mississippi", "pi");
+            // model.start("hello", "ll");
+            // t.Stop();
+            // Console.WriteLine($"front:{t.ElapsedMilliseconds}/ms");
+            // t.Restart();
+            // model.sample("mississippi", "pi");
+            // model.sample("hello", "ll");
+            // t.Stop();
+            // Console.WriteLine($"back:{t.ElapsedMilliseconds}/ms");
+            Console.WriteLine($"Result:{new InsertPosition().start(new int[] { 1,2,3,4,5,10}, 2)}");
         }
     }
 }
