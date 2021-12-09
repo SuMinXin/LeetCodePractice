e93ffe5999ad948b6d4539e89b678b1bf629efeb
MinxinSU <sian415@gmail.com>
2021-12-09 19:58:13 +0800

    PlusOne

---
+++ PlusOne.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCode {
+    public class PlusOne {
+        public int[] start(int[] digits) {
+            int len = digits.Length;
+            int[] one = new int[len + 1];
+            bool add = false;
+            for (int i = len - 1; i >= 0; i--) {
+                if (digits[i] != 9) {
+                    digits[i] = digits[i] + 1;
+                    add = false;
+                    break;
+                } else {
+                    digits[i] = 0;
+                    add = true;
+                }
+                one[i] = 0;
+            }
+            if (add) {
+                one[0] = 1;
+                return one;
+            }
+            return digits;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,15 +10,8 @@ namespace LeetCode {
             //new RemoveElement().start(new int[] { 2 }, 3);
             // Stopwatch t = new Stopwatch();
             // StrString model = new StrString();
-            // t.Start();
             // model.start("mississippi", "pi");
-            // model.start("hello", "ll");
-            // t.Stop();
-            // Console.WriteLine($"front:{t.ElapsedMilliseconds}/ms");
-            // t.Restart();
-            // model.sample("mississippi", "pi");
             // model.sample("hello", "ll");
-            // t.Stop();
             // Console.WriteLine($"back:{t.ElapsedMilliseconds}/ms");
             Console.WriteLine($"Result:{new InsertPosition().start(new int[] { 1,2,3,4,5,10}, 2)}");
         }
